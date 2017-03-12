badda337407e44b872b23af58334e4c1fb1903fe
Aldrin Ronco <aldrin.ronco@gmail.com>
2017-03-12 16:50:40 -0500

    adding customer logo

--- src/components/login-setup.vue
+++ src/components/login-setup.vue
@@ -13,7 +13,7 @@ export default {
     this.$store.commit('SET_HOST_PWD', this.$route.query.pwd)
     this.$store.commit('SET_HOST_ID', this.$route.query.id)
     this.$store.commit('SET_COMPANY_LOGO', this.$route.query.logo)
-    console.log(this.$route.query.logo)
+    console.log(this.$route.query)
     this.$router.push('/login') // Redirección a ruta de Login
   },
   computed: {
